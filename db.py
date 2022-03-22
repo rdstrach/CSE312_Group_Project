a2048022cf954f48bfddbc8f3c0cc55c6fa331e4
Ryan <rdstrach@buffalo.edu>
2022-03-22 10:25:52 -0400

    Plan to seperate this file, database file, login file and registration file

--- db.py
+++ db.py
@@ -5,95 +5,21 @@ from werkzeug.security import check_password_hash
 import re
 import json
 mongo_client = MongoClient('mongo', 27017)
-db_version = 1
-account_db = mongo_client["account_db"+str(db_version)]
+version = 1
+"""
+account_db: contains 2 collections, user account info,
+users_id_collection: collection for id increment. Only used by next_id
+"""
+account_db = mongo_client["account_db"+str(version)]
 users_id_collection = account_db["id_counter"]
 account_info = account_db["account_info"]
 id_query = {"field": "key"}
 
-#registration content
-def db_get_content(some_data_collection):
-    cur = some_data_collection.find()
-    not_first = 0
-    ret = "["
-    for i in cur:
-        if not_first == 1:
-            ret += ","
-        not_first = 1
-        ret += "{"
-        ret += "\"id\":" + str(i["id"])
-
-        ret += ",\"firstname\":\"" + i["firstname"]
-        ret += "\""
-
-        ret += ",\"lastname\":\"" + i["lastname"]
-        ret += "\""
-
-        ret += ",\"username\":\""+ i["username"]
-        ret += "\""
-
-        ret += ",\"password\":\"" + i["password"]
-        ret += "\""
-        ret += "}"
-    ret += "]"
-    ret_len = len(ret)
-
-    if ret_len == 1:
-        ret="[{}]"
-    return ret
-
-def next_id():
-
-    size = users_id_collection.count_documents(id_query)
-    if size == 0:
-        firstId = 1
-        users_id_collection.insert_one({'id': firstId,'field':'key'})
-        return firstId
-    else:
-        current_id = users_id_collection.find(id_query)[0]['id']
-        nextId = current_id + 1
-        newvalues = {"$set": {"id": nextId}}
-        users_id_collection.update_one(id_query,newvalues)
-        return nextId
-def user_exist(username):
-
-    query = {"username": username}
-    data = list(account_info.find(query, {"_id": False, "id": True, "username": True}))
-    return len(data) > 0
-
-
-def validate_registration_input(first_name, last_name, username, password, password_again):
-    ret=set()
-    username = str(username).lower()
-    if not str(first_name).isalpha():
-        ret.add("First name can only contain alphabetic characters")
-    if not str(last_name).isalpha():
-        ret.add("Last name can only contain alphabetic characters")
-    if len(first_name) == 0:
-        ret.add("No user input for first name.")
-    if len(last_name) == 0:
-        ret.add("No user input for last name.")
-    if user_exist(username):
-        ret.add("Username already exist.")
-    if len(username) == 0:
-        ret.add("No user input for username.")
-    if len(password) == 0:
-        ret.add("No user input for Password.")
-    if len(password) < 8:
-        ret.add("Password must be at least 8 characters long.")
-    if password != password_again:
-        ret.add("Passwords do not match")
-    return ret
-def password_hash_gen(password):
-    try:
-        hash = generate_password_hash(password, "sha256")
-    except:
-        print("error with hash")
-
-    if check_password_hash(hash,password) and hash != password:
-        return hash
-    else:
-        return 0
+"""
+create_user(first_name,last_name,username,password,password_again)
+returns empty set if userdata valid and created
+returns set of strings, with user error description 
+"""
 def create_user(first_name,last_name,username,password,password_again):
     data = dict()
     val_ret=set()
@@ -118,39 +44,138 @@ def create_user(first_name,last_name,username,password,password_again):
     else:
         return val_ret
 
+"""
+def validate_registration_input(first_name, last_name, username, password, password_again)
+returns empty set if user data is valid
+returns set of strings, with user error description 
+
+No changes to database done here
+"""
+
+def validate_registration_input(first_name, last_name, username, password, password_again):
+    ret=set()
+    username = str(username).lower()
+    if not str(first_name).isalpha():
+        ret.add("First name can only contain alphabetic characters")
+    if not str(last_name).isalpha():
+        ret.add("Last name can only contain alphabetic characters")
+    if len(first_name) == 0:
+        ret.add("No user input for first name.")
+    if len(last_name) == 0:
+        ret.add("No user input for last name.")
+    if user_exist(username):
+        ret.add("Username already exist.")
+    if len(username) == 0:
+        ret.add("No user input for username.")
+    if len(password) == 0:
+        ret.add("No user input for Password.")
+    if len(password) < 8:
+        ret.add("Password must be at least 8 characters long.")
+    if password != password_again:
+        ret.add("Passwords do not match")
+    return ret
+
+"""
+def next_id()
+Is is uses users_id_collection to count id's; will increment and return id
+Called only for user_create()
+req1-note: undefined behaviour if this value is nonpositive
+
+req2-note: undefined behaviour if this value is nonpositive
+
+"""
+def next_id():
+
+    size = users_id_collection.count_documents(id_query)
+    if size == 0:
+        firstId = 1  # should  be a positive start value req1-note
+        users_id_collection.insert_one({'id': firstId,'field':'key'})
+        return firstId
+    else:
+        current_id = users_id_collection.find(id_query)[0]['id']
+        id_distance = 1 # this value must be a positive value req2-note
+        nextId = current_id + id_distance
+        newvalues = {"$set": {"id": nextId}}
+        users_id_collection.update_one(id_query,newvalues)
+        return nextId
 
 
+
+
+
+"""
+ login_validation(username, password)
+ returns distinct positive ..integer type.. value "user id" if given correct username and password
+ or returns -1 if otherwise
+"""
 
 
 def login_validation(username, password):
-
+    username=str(username).lower()
     query = {"username": username}
     data = list(account_info.find(query, {"_id": False, "id": True, "username": True, "password": True}))
 
     if len(data) != 1:
-        return False
+        return -1
 
 
     jsonString = re.sub("\'", "\"", str(data[0]))
     dataDict = json.loads(jsonString)
 
     if check_password_hash(dataDict["password"], password):
-        return True
+        return dataDict["id"]
     else:
-        return False
+        return -1
 
-def user_data(username):
+"""
+ def user_data(id)
+    returns dictionary of all user data except password give the user id as integer
+    returns bool FALSE if id doesn't exist, nonpositive values will return false,
+     as long as next_id() begins at the value 1
+"""
+def user_data(id):
 
-    query = {"username": username}
+    query = {"id": id}
     data = list(account_info.find(query,{"_id": False,"password":False}))
 
 
     if len(data) != 1:
         return False
     jsonString = re.sub("\'", "\"", str(data[0]))
-    #flush("json: "+str(jsonString))
 
     userdata = json.loads(jsonString)
 
     return userdata #return userdata as dictionary
 
+"""
+def user_exist(username):
+returns true if username already exist; username is not considered to be case sensitive;
+used in validate_registration_input function to ensure there are no duplicate usernames
+
+"""
+
+
+def user_exist(username):
+    username=str(username).lower()
+    query = {"username": username}
+    data = list(account_info.find(query, {"_id": False, "id": True, "username": True}))
+    return len(data) > 0
+
+
+"""
+def password_hash_gen(password)
+return password hash or 0 if hash parameter is typed in incorrectly 
+
+"""
+
+
+def password_hash_gen(password):
+    try:
+        hash = generate_password_hash(password, "sha256")
+    except:
+        print("error with hash")
+
+    if check_password_hash(hash,password) and hash != password:
+        return hash
+    else:
+        return 0
