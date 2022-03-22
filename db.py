8cc6e8eef446e1cb90376c25fa14fe78af463b7d
Ryan <rdstrach@buffalo.edu>
2022-03-22 11:52:32 -0400

    Plan to seperate this file, database file, login file and registration file

--- db.py
+++ db.py
@@ -25,12 +25,14 @@ else
 no data modified/added to database
 returns set of string(s), with user error descriptions, 
 """
-def create_user(first_name,last_name,username,password,password_again):
+
+
+def create_user(first_name, last_name, username, password, password_again):
     data = dict()
     val_ret=set()
     username= str(username).lower()
     val_ret = validate_registration_input(first_name, last_name, username, password, password_again)
-    if (len(val_ret)!=0):
+    if len(val_ret) != 0:
         return val_ret
 
     data["firstname"] = first_name
@@ -49,16 +51,18 @@ def create_user(first_name,last_name,username,password,password_again):
     else:
         return val_ret
 
+
 """
 def validate_registration_input(first_name, last_name, username, password, password_again)
 returns empty set if user data is valid
 returns set of strings, with user error descriptions
 
-No changes to made to database in this function
+No changes made to database in this function
 """
 
+
 def validate_registration_input(first_name, last_name, username, password, password_again):
-    ret=set()
+    ret = set()
     username = str(username).lower()
     if not str(first_name).isalpha():
         ret.add("First name can only contain alphabetic characters")
@@ -80,33 +84,6 @@ def validate_registration_input(first_name, last_name, username, password, passw
         ret.add("Passwords do not match")
     return ret
 
-"""
-def next_id()
-Is is uses users_id_collecteion to count id's; will increment and return id
-Called only for user_creat()
-
-req1-note: undefined behaviour if this value is nonpositive
-req2-note: undefined behaviour if this value is nonpositive
-
-"""
-def next_id():
-
-    size = users_id_collection.count_documents(id_query)
-    if size == 0:
-        firstId = 1  # should  be a positive start value req1-note
-        users_id_collection.insert_one({'id': firstId,'field':'key'})
-        return firstId
-    else:
-        current_id = users_id_collection.find(id_query)[0]['id']
-        id_distance = 1 # this value must be a positive value req2-note
-        nextId = current_id + id_distance
-        newvalues = {"$set": {"id": nextId}}
-        users_id_collection.update_one(id_query,newvalues)
-        return nextId
-
-
-
-
 
 """
  login_validation(username, password)
@@ -116,21 +93,21 @@ def next_id():
 
 
 def login_validation(username, password):
-    username=str(username).lower()
+    username = str(username).lower()
     query = {"username": username}
     data = list(account_info.find(query, {"_id": False, "id": True, "username": True, "password": True}))
 
     if len(data) != 1:
         return -1
 
+    json_string = re.sub("\'", "\"", str(data[0]))
+    data_dict = json.loads(json_string)
 
-    jsonString = re.sub("\'", "\"", str(data[0]))
-    dataDict = json.loads(jsonString)
-
-    if check_password_hash(dataDict["password"], password):
-        return dataDict["id"]
+    if check_password_hash(data_dict["password"], password):
+        return data_dict["id"]
     else:
         return -1
+
 
 """
  def user_data(id)
@@ -138,20 +115,22 @@ def login_validation(username, password):
     returns bool FALSE if id doesn't exist, nonpositive values will return false
      
 """
+
+
 def user_data(id):
     if id <= 0:
         return False
     query = {"id": id}
     data = list(account_info.find(query,{"_id": False,"password":False}))
 
-
     if len(data) != 1:
         return False
-    jsonString = re.sub("\'", "\"", str(data[0]))
+    json_string = re.sub("\'", "\"", str(data[0]))
 
-    userdata = json.loads(jsonString)
+    userdata = json.loads(json_string)
 
-    return userdata #return userdata as dictionary
+    return userdata  # return userdata as dictionary
+
 
 """
 def user_exist(username):
@@ -162,7 +141,7 @@ query user
 
 
 def user_exist(username):
-    username=str(username).lower()
+    username = str(username).lower()
     query = {"username": username}
     data = list(account_info.find(query, {"_id": False, "id": True, "username": True}))
     return len(data) > 0
@@ -177,11 +156,40 @@ return password hash or 0 if hash parameter is typed in incorrectly
 
 def password_hash_gen(password):
     try:
-        hash = generate_password_hash(password, "sha256")
+        hash_password = generate_password_hash(password, "sha256")
     except:
-        print("error with hash")
-
-    if check_password_hash(hash,password) and hash != password:
-        return hash
-    else:
+        print("Error with hash")
         return 0
+
+    if check_password_hash(hash_password, password) and hash_password != password:
+        return hash_password
+
+    return 0
+
+
+"""
+def next_id()
+uses collection users_id_collection to count id's; will increment and return id
+Called only for user_create()
+
+req1-note: undefined behaviour if this value is non-positive
+req2-note: undefined behaviour if this value is non-positive
+
+"""
+
+
+def next_id():
+
+    size = users_id_collection.count_documents(id_query)
+    if size == 0:
+        firstId = 1  # should  be a positive start value... req1-note
+        users_id_collection.insert_one({'id': firstId,'field':'key'})
+        return firstId
+    else:
+        current_id = users_id_collection.find(id_query)[0]['id']
+        id_distance = 1 # this value must be a positive value... req2-note
+        nextId = current_id + id_distance
+        newvalues = {"$set": {"id": nextId}}
+        users_id_collection.update_one(id_query,newvalues)
+        return nextId
+
